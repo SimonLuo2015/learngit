0e9940662ec6f09eb0592060b2cfae018091a163
SimonLuo2015 <luoshujian1987@163.com>
2016-09-27 00:05:12 +0800

    add sizer notes for InitUI()

--- hipython-master/custom_dialogs_test.py
+++ hipython-master/custom_dialogs_test.py
@@ -40,25 +40,44 @@ class ChangeDepthDialog(wx.Dialog):
 
         # 下面的-1表示使用默认分配的id
         sbs.Add(wx.StaticText(pnl, -1, r'Start Date'))
-        self.dc_start = wx.DatePickerCtrl(pnl, -1, style=wx.DP_DROPDOWN, pos=(130, 70))
+        # 返回DatePickerCtrl。
+        self.dc_start = wx.DatePickerCtrl(pnl, -1, style=wx.DP_DEFAULT, pos=(130, 70))
+        # 将dc_start添加到sbs中。
         sbs.Add(self.dc_start)
+
+
         sbs.Add(wx.StaticText(pnl, -1, r'End Date'))
         self.dc_end = wx.DatePickerCtrl(pnl, -1, style=wx.DP_DROPDOWN, pos=(330, 70))
         sbs.Add(self.dc_end)
+
+        # 将sbs关联到容器pnl面板。因为box sizer要嵌套，而box sizer只能作用与一个panel中。
         pnl.SetSizer(sbs)
 
-        # 再新建一个panel？？为什么要新建？
+        # 再新建一个panel，重新应用一个box sizer。
         pnl2 = wx.Panel(self)
+        # 新建sb2的静态框
         sb2 = wx.StaticBox(pnl2, label='Data Set')
-        sbs2 = wx.StaticBoxSizer(sb2, orient=wx.VERTICAL)        
+        # 基于sb2的静态框，新建一个静态box sizer布局。
+        sbs2 = wx.StaticBoxSizer(sb2, orient=wx.VERTICAL)
+
+        # 创建一组复选框，cb是check box的缩写。
         self.cb_list = []
         for l in self.option_list:
-            cb = wx.CheckBox(pnl2, label = l) # originally 3 params: style=wx.RB_GROUP
+            # 新建一个cb的复选框。
+            # wx.CheckBox(parent, id, label, pos=wx.DefaultPosition,
+            #   size=wx.DefaultSize, style=0, name="checkBox")
+            cb = wx.CheckBox(pnl2, -1, l) # originally 3 params: style=wx.RB_GROUP
+            # 将复选框加入到sb2的布局中。
             sbs2.Add(cb)
+            # 将cb复选框实例加入到列表。有什么作用？
             self.cb_list.append(cb)
-        
+
+        # 将sbs2的静态框布局关联到新建的容器pnl2面板上。
         pnl2.SetSizer(sbs2)
+
         # 在一个dialog下，有两个panel面板？？然后使用hbox来将两个panel进行横向排列？
+        # 将两个面板加入到bhox的纵向box sizer中。
+        # pnl与pnl2是
         hbox.Add(pnl)
         hbox.Add(pnl2)
        
@@ -72,6 +91,8 @@ class ChangeDepthDialog(wx.Dialog):
         vbox.Add(hbox2, 
             flag=wx.ALIGN_CENTER|wx.TOP|wx.BOTTOM, border=10)
 
+        # 看到了没有，这里是self被关联到vbox这个布局。
+        # 这个box sizer是最高级的，其他的box sizer都是和下面的panel这些小的容器关联，都是下级。
         self.SetSizer(vbox)
         
         okButton.Bind(wx.EVT_BUTTON, self.OnClose)
