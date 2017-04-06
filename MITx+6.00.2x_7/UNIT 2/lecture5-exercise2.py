559105db68f433172cf90da45fc9ba2ae379c09f
SimonLuo2015 <luoshujian1987@163.com>
2017-04-06 11:44:18 +0800

    add lecture5 exercise2 codes

---
+++ MITx+6.00.2x_7/UNIT 2/lecture5-exercise2.py
@@ -0,0 +1,19 @@
+"""
+Create on 04/06/2017
+@author: Simon
+"""
+
+import random
+def genEven():
+    '''
+    Returns a random even number x, where 0 <= x < 100 
+    '''
+    return random.randrange(0, 100, 2)
+
+def testGenEven(n=100):
+    result = ''
+    for i in range(n):
+        result += str(genEven()) + '-'
+    return result
+
+print testGenEven()
